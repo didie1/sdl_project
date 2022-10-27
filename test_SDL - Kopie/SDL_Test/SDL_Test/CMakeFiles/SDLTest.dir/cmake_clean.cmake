87c627a23959a4e0d208e02e90753d924ada56e1
Immaculée BAHOUNDJE <immabhd@gmail.com>
2022-10-27 17:04:05 +0200

    add Sdl

---
+++ test_SDL - Kopie/SDL_Test/SDL_Test/CMakeFiles/SDLTest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SDLTest.dir/Main.cpp.o"
+  "SDLTest"
+  "SDLTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SDLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
