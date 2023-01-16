e070a88df0bb57221f731cb76d703db6810ba718
imma-bhdje <114417737+imma-bhdje@users.noreply.github.com>
2023-01-16 16:36:30 +0100

    Update main.cpp

--- Project_SDL_final/Project_SDL_base/main.cpp
+++ Project_SDL_final/Project_SDL_base/main.cpp
@@ -20,11 +20,11 @@ int main(int argc, char* argv[]) {
 
   application my_app = application(std::stoul(argv[1]), std::stoul(argv[2]));
 
-  # std::cout << "Window" << std::endl;
+  // std::cout << "Window" << std::endl;
 
   int loop_val = my_app.loop(std::stoul(argv[3]));
 
-   # std::cout << "Exiting" << loop_val << std::endl;
+   // std::cout << "Exiting" << loop_val << std::endl;
 
   SDL_Quit();
 
